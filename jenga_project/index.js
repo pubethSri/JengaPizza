fc5e4226f3578afaefc94167c9ade9be5850c1cb
CruzSri <66070158@kmitl.ac.th>
2025-03-02 19:47:58 +0700

    Implement login session with express-session, add sample users, and set up login/logout functionality
    - Installed express-session to manage login sessions in Express.js
    - Added sample users (admin and Cruz) to the SQLite database
    - Implemented login and logout functionality
    - Restricted access to the order page for non-logged-in users
    - Updated login button to toggle between "Login" and "Logout" based on session state

--- jenga_project/index.js
+++ jenga_project/index.js
@@ -1,10 +1,33 @@
 const express = require("express");
 const path = require("path");
 const port = 3000;
+const bodyParser = require("body-parser");
+const sqlite3 = require('sqlite3').verbose();
+const session = require('express-session');
+
+// Connect to SQLite database
+let db = new sqlite3.Database("data/pizzeria.db", (err) => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log('Connected to the Pizzeria database.');
+});
+
 
 // Creating the Express server
 const app = express();
 
+
+// start session i guess?????
+app.use(session({
+  secret: 'secret',
+  resave: true,
+  saveUninitialized: true
+}));
+
+app.use(express.json());
+
+app.use(bodyParser.urlencoded({ extended: true }));
 // static resourse & template engine
 app.use(express.static("views"));
 
@@ -12,15 +35,19 @@ app.use(express.static("views"));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-    res.render('Landing');
+  res.render('Landing');
 });
 
 app.get("/home", (req, res) => {
-  res.render('home');
+  res.render('home', { loggedin: req.session.loggedin });
 });
 
 app.get("/choose", (req, res) => {
-  res.render('choose');
+  if (req.session.loggedin) {
+    res.render('choose', { loggedin: req.session.loggedin });
+  } else {
+    res.redirect('/home');
+  }
 });
 
 app.get("/category", (req, res) => {
@@ -30,6 +57,33 @@ app.get("/category", (req, res) => {
 app.get("/pizza-name", (req, res) => {
   res.render('pizza-name');
 });
+
+app.post("/authen", async (req, res) => {
+  const { username, password } = req.body;
+  const sql = `SELECT * FROM users WHERE username = "${username}" AND user_password = "${password}"`
+  db.all(sql, (error, results) => {
+    if (error) {
+      console.log(error.message);
+    }
+    if (results.length > 0) {
+      req.session.loggedin = true;
+      req.session.username = username;
+      console.log("logged in!");
+      res.redirect('/home');
+    } else {
+      res.send("incorrect password and/or password!");
+    }
+    res.end();
+  })
+})
+
+app.get("/logout", (req, res) => {
+  req.session.destroy();
+  // console.log(req.session.loggedin);
+  // console.log(req.session.username);
+  console.log("logged out!");
+  res.redirect('/home');
+})
 
 app.listen(port, () => {
   console.log(`This Web Server is running on port ${port}`);
