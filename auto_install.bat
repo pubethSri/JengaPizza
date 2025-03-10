74560cb3e15f1ce61e36fa1b9e3471dca951b137
FriedStan <66070030@kmitl.ac.th>
2025-03-10 14:01:00 +0700

    Change some wording in batch script

--- auto_install.bat
+++ auto_install.bat
@@ -1,11 +1,9 @@
 @echo off
-echo Install Warming up Jenga pizza's oven.........
+echo Warming up Jenga pizza's oven.......
 
-:: Second window - MNPCore with npm i and npm run dev
 start powershell.exe -NoExit -Command "cd ./jenga_project; npm i; npm run dev"
 
 timeout /t 4
 
 start http://localhost:3000
-
-echo Browser opened to localhost:3000
+echo Jenga Pizza is now open and ready to serve!
